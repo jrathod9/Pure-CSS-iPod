b8722e05c0ba91bb60771ceb15eed37e93b4083e
jrathod9 <lewisburnard@gmail.com>
2019-02-04 12:15:51 +0530

    interaction update

---
+++ func.js
@@ -0,0 +1,8 @@
+function printname(){
+  document.getElementById('creator').innerHTML= "Jay Rathod";
+  document.getElementById('creator').style["text-align"]= "center";
+}
+function restoreback(){
+  document.getElementById('creator').innerHTML= "Creator";
+  document.getElementById('creator').style["text-align"]= "left";
+}
